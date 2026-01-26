55de08ac01d38f5bf30ad957d3eda49a3f9a49d9
Tech Rules <66055840+techrules07@users.noreply.github.com>
2026-01-26 19:59:26 +0530

    initial push for the console

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Index from './components/Login/Index'
+import Verify from './components/Login/Verify';
+import Home from './components/dashboard/Home';
+
+function App() {
+ 
+  
+
+  return (
+    <Router>
+        <Routes>
+      <Route path='/' element={ <Index />}/>
+      <Route path='/verify' element={<Verify />} />
+      <Route path='/home' element={<Home />} />
+    </Routes>
+    </Router>
+    
+  )
+}
+
+export default App
